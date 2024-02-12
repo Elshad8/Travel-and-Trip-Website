545bda89f1bfeafc5e0f4da4b3af52a2f6426291
Elshad Feyzili <elshad.feyzili@gmail.com>
2024-02-12 12:02:30 +0400

    travel_trip_first_commit

---
+++ Controllers/AboutController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TravelTripProje_v2.Models.Siniflar;
+namespace TravelTripProje_v2.Controllers
+{
+    public class AboutController : Controller
+    {
+        // GET: About
+        Context c = new Context();
+        public ActionResult Index()
+        {
+            var degerler = c.Hakkimizdas.ToList();
+            return View(degerler);
+        }
+    }
+}
